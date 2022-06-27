92474d851ffefeec2911142daaff3960a4eeaef2
garfnadeck <serraopinto@gmail.com>
2022-06-27 16:27:50 -0300

    feature: add server api

---
+++ api/server.go
@@ -0,0 +1,34 @@
+package api
+
+import (
+	db "github.com/garfnadeck/go-finance/db/sqlc"
+	"github.com/gin-gonic/gin"
+)
+
+type Server struct {
+	store  *db.SQLStore
+	router *gin.Engine
+}
+
+func NewServer(store *db.SQLStore) *Server {
+	server := &Server{store: store}
+	router := gin.Default()
+
+	// routas
+
+	router.POST("/user", server.createUser)
+	router.GET("/user/:username", server.getUser)
+	router.GET("/user/:id", server.getUserById)
+
+	server.router = router
+	return server
+
+}
+
+func (server *Server) Start(address string) error {
+	return server.router.Run(address)
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{"api has an error:": err.Error()}
+}
